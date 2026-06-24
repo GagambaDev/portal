5f2095468e689b348b10ccd61f319b6d1aa5e7ba
Dev Ada <250013605+ada-the-dev@users.noreply.github.com>
2026-06-23 23:43:47 -0700

    Cards are responsive to 821px and above. Also, the size should never shrink below 280px too. Added comment explaining responsiveness change.

--- components/portfolio/PortfolioCardGrid.tsx
+++ components/portfolio/PortfolioCardGrid.tsx
@@ -10,7 +10,10 @@ export default function PortfolioCardGrid({
   properties,
 }: PortfolioCardGridProps) {
   return (
-    <div className="grid-container">
+    // Cards will have a single-column grid layout by default until width hits 821 pixels.
+    // When this happens, program will do the math to figure out how many cards can fit in a row. 
+    // Spacing between the cards will remain consistent thanks to gap-5.
+    <div className="grid grid-cols-1 min-[821px]:grid-cols-[repeat(auto-fill,minmax(280px,auto))] gap-5">
       {(properties || []).map((property) => (
         <PortfolioCard key={property.id} property={property} />
       ))}
