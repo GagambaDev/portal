ed50626d4238a4288a744dd87e712c70d62875af
Adrain O. <aortega0610.ao@gmail.com>
2026-06-26 01:35:06 -0700

    Implemented the PDFExportButton component. Wired it tp exportPdf and gave it a bit of style

--- components/ui/pdf-export-button.tsx
+++ components/ui/pdf-export-button.tsx
@@ -1,19 +1,33 @@
-<<<<<<< HEAD
-"use client"
+"use client";
 
-import { useState, useRef, RefObject } from "react"
-import { Button } from "@/components/ui/button"
-import { exportPDF } from "@/lib/export-pdf"
-import { toast } from "sonner"
+import { useState, RefObject } from "react";
+import { Button } from "@/components/ui/button";
+import { exportPdf } from "@/lib/export-pdf";
+import { FileDown } from "lucide-react"
 
-interface PDFExportButtonProps {
-    
-    targetRef: RefObject<HTMLElement>
-    buildingName: string
+interface PDFExportButtonProps { 
+    reportRef: RefObject<HTMLElement | null>;
+    buildingName: string;
 }
-export default function PDFExportButton({
-    target 
-
-}:
-
-)
+export function PdfExportButton({ reportRef, buildingName} : PDFExportButtonProps) {
+    const [isLoading, setIsLoading] = useState(false);
+    
+    async function onClick() {
+        {/* ref is null before the component mounts*/}
+        if (!reportRef.current) return;
+        setIsLoading(true);
+        await exportPdf(reportRef.current, buildingName);
+        setIsLoading(false);
+    }
+    return ( 
+        <Button
+            className="text-[#D5D2F7] bg-[#583FD4] hover:bg-[#583FD4] 
+            hover:ring-1 hover:ring-[#00AAFF]"
+            onClick={onClick} 
+            disabled={isLoading}
+        >
+        <FileDown/>
+            Download PDF
+        </Button>
+    );
+}
