c1a31b610ed2f1e503f2a0be4bb7c9af7d36e8db
Dev Ada <250013605+ada-the-dev@users.noreply.github.com>
2026-06-28 09:36:12 -0700

    Imported the skeleton single card, so we can map this card multiple times. Created the loading card grid

--- components/portfolio/PortfolioCardGrid.tsx
+++ components/portfolio/PortfolioCardGrid.tsx
@@ -5,6 +5,7 @@
 
 import PortfolioCard from './PortfolioCard'
 import type { PortfolioCardGridProps } from '@/lib/types'
+import {LoadingCard} from './PortfolioCard'
 
 export default function PortfolioCardGrid({
   properties,
@@ -16,6 +17,27 @@ export default function PortfolioCardGrid({
     <div className="grid grid-cols-1 min-[821px]:grid-cols-[repeat(auto-fill,minmax(280px,auto))] gap-5">
       {(properties || []).map((property) => (
         <PortfolioCard key={property.building_id} property={property} />
+      ))}
+    </div>
+  );
+}
+
+export function LoadingCardGrid() {
+  // Create an empty array that has 10 elements.
+  // We will use this to simulate the index value of a for-loop.
+  const LoadingCards = Array.from({ length: 10});
+  
+  return (
+    // Cards will have a single-column grid layout by default until width hits 821 pixels.
+    // When this happens, program will do the math to figure out how many cards can fit in a row. 
+    // Spacing between the cards will remain consistent thanks to gap-5.
+    <div className="grid grid-cols-1 min-[821px]:grid-cols-[repeat(auto-fill,minmax(280px,auto))] gap-5">
+      
+      {/* We now will store loading cards in an array that holds html elements that
+      can render on our screen.
+      For every item in the empty array, generate a loading card into our collection. */}
+      {LoadingCards.map((_, index) => (
+        <LoadingCard key={index} />
       ))}
     </div>
   );
